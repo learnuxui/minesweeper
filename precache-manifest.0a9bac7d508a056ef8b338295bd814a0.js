c3fb96d16fc2d2226aecf276401b3359f1b02e4b
unknown <vaibhavtekade.study@gmail.com>
2020-08-12 22:37:01 +0530

    Updates

---
+++ precache-manifest.0a9bac7d508a056ef8b338295bd814a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d87861f6d273906737a984826ac4e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "760bdb13b08084a99120",
+    "url": "/static/css/main.7f9ed4fa.chunk.css"
+  },
+  {
+    "revision": "85db4c32c0bd3adf2020",
+    "url": "/static/js/2.80ca414e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.80ca414e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760bdb13b08084a99120",
+    "url": "/static/js/main.c0ec5e35.chunk.js"
+  },
+  {
+    "revision": "5bf75b294f1ce371b4bc",
+    "url": "/static/js/runtime-main.c056d1e7.js"
+  }
+]);

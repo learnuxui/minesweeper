486486d78d8e26e127e0f26b120a8c2e9b38ca95
unknown <vaibhavtekade.study@gmail.com>
2020-08-12 22:40:19 +0530

    Updates

---
+++ precache-manifest.c5fd9c16efa38fda29a854a15aade692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc1c31c909374f7b65866fb1d6c35e6",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "760bdb13b08084a99120",
+    "url": "/minesweeper/static/css/main.7f9ed4fa.chunk.css"
+  },
+  {
+    "revision": "85db4c32c0bd3adf2020",
+    "url": "/minesweeper/static/js/2.80ca414e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minesweeper/static/js/2.80ca414e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760bdb13b08084a99120",
+    "url": "/minesweeper/static/js/main.c0ec5e35.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
